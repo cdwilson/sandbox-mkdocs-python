c0132a654276785b31e18fb54a87b61466465ab0
Chris Wilson <christopher.david.wilson@gmail.com>
2024-08-09 00:10:25 -0700

    Remove __about__.py

--- src/sandbox_mkdocs_python/__init__.py
+++ src/sandbox_mkdocs_python/__init__.py
@@ -1,3 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present Chris Wilson <christopher.david.wilson@gmail.com>
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
+
+"""This is the module docstring for the sandbox_mkdocs_python module."""
+
+__version__ = "0.1.0"
