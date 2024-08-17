c059041ca3be9b39aaa4887e3772c73cb4d5de4e
Chris Wilson <christopher.david.wilson@gmail.com>
2024-08-16 23:54:21 -0700

    Test inherited_members

---
+++ src/sandbox_mkdocs_python/module.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2024-present Chris Wilson <christopher.david.wilson@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+"""Module docstring."""
+
+
+class Base:
+    """Base class."""
+
+    def base(self):
+        """Base method."""
+
+
+class Main(Base):
+    """Main class."""
+
+    def main(self):
+        """Main method."""
---
+++ src/sandbox_mkdocs_python/module_pydantic.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2024-present Chris Wilson <christopher.david.wilson@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+"""Module docstring."""
+
+from pydantic import BaseModel
+
+
+class Main(BaseModel):
+    """Main class."""
+
+    def main(self):
+        """Main method."""
